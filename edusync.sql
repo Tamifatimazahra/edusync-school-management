b79cac414182a1bb849fe07cce164a4b42c1fa22
Tamifatimazahra <tamifatimazahra57@gmail.com>
2026-04-16 12:51:14 +0200

    create database and tables

---
+++ edusync.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE edusync;
+USE edusync;
+CREATE TABLE roles(id_roles int  AUTO_INCREMENT PRIMARY KEY ,
+label VARCHAR(50)  );
+SELECT * from roles;
+CREATE TABLE users(id_users int AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(50),
+lastname VARCHAR(50),
+id_roles INT,
+FOREIGN KEY(id_roles)REFERENCES roles(id_roles));
+SELECT * FROM users;
+CREATE TABLE courses(id_courses INT AUTO_INCREMENT PRIMARY KEY ,
+title VARCHAR(50),
+)
